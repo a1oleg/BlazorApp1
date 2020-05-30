cae4f33d19588a62694aec8d7571741a4b76e77e
a1oleg <a1olegs@gmail.com>
2020-05-30 10:36:51 +0300

    без кнопки submit не работает
    пробовать онклик на каждом

--- Data/ExampleModel.cs
+++ Data/ExampleModel.cs
@@ -3,6 +3,6 @@
 public class ExampleModel
 {
     [Required]
-    [StringLength(10, ErrorMessage = "Name is too long.")]
+    //[StringLength(10, ErrorMessage = "Name is too long.")]
     public string Name { get; set; }
 }
