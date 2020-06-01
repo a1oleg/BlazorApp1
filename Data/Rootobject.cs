9da1d39ce6804564434d5dce66e0d8f8a8b5342e
a1oleg <a1olegs@gmail.com>
2020-06-01 15:53:41 +0300

    сервер говорит BadRequest на переметризированные запросы

--- Data/Rootobject.cs
+++ Data/Rootobject.cs
@@ -11,14 +11,26 @@ namespace BlazorApp1.Data
 
     public class Data
     {
-        public object Values { get; set; }
-        public List<string> Dirs { get; set; }
-        public List<Dir> Dir { get; set; }
+        //public object Values { get; set; }
+        //public List<string> Dirs { get; set; }
+        //public List<Dir> Dir { get; set; }
+        public Dir Dir { get; set; }
     }
     public class Dir
     {
         public string description { get; set; }
-        public List<string> Values { get; set; }
+        public List<Value> Values { get; set; }
+    }
+
+    public class Value
+    {
+        public string TXTLG { get; set; }
+        public List<Wagon> Wagons { get; set; }
+    }
+    public class Wagon
+    {
+        public int _id { get; set; }
+        public List<Value> Values { get; set; }
     }
 
 }
--- Data/ValuesService.cs
+++ Data/ValuesService.cs
@@ -11,31 +11,18 @@ namespace BlazorApp1.Data
 {
     public class ValuesService
     {        
-        public async Task<Rootobject> GetAsync(string route)
+        public async Task<Rootobject> GetAsync(GraphQLRequest nrequest)
         {
             string local = "http://localhost:4001/graphql";
 
             GraphQLHttpClient graphQLClient = new GraphQLHttpClient(local, new NewtonsoftJsonSerializer());
-
-
-            GraphQLRequest request = new GraphQLRequest
-            {                
-                Query = route//,
-       //         @"
-			    //query Dirs {
-       //               Dirs
-       //           }",
-                //OperationName = "Dirs"
-            };
-
-
 
             JsonSerializerOptions jso = new JsonSerializerOptions
             {
                 Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
             };
 
-            var graphQLResponse = await graphQLClient.SendQueryAsync<Data>(request);
+            var graphQLResponse = await graphQLClient.SendQueryAsync<Data>(nrequest);
             graphQLClient.Dispose();
             //return System.Text.Json.JsonSerializer.Serialize(graphQLResponse, jso);
             return JsonConvert.DeserializeObject<Rootobject>(System.Text.Json.JsonSerializer.Serialize(graphQLResponse, jso));
